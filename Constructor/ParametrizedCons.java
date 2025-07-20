84e7c35ae7aea9ff3146f65b2911d061e78d23b3
Shishu Pal <shishupalkr562@gmail.com>
2025-07-20 09:46:58 +0530

    First

---
+++ Constructor/ParametrizedCons.java
@@ -0,0 +1,27 @@
+package Constructor;
+
+import javax.naming.PartialResultException;
+
+class ParametrizedCons {
+    String name;
+    int rollNo;
+    // Create Parametrized Constructor 
+    ParametrizedCons(String n, int r)
+    {
+        name = n;
+        rollNo = r;
+    }
+    void display()
+    {
+        System.out.println("Name : "+name);
+        System.out.println("Roll No. : "+rollNo);
+    }
+    public static void main(String[] args) {
+        ParametrizedCons p1 = new ParametrizedCons("Shishu Pal", 101);
+        ParametrizedCons p2 = new ParametrizedCons("Ayush Raj", 102);
+
+        // Call the method for Show
+        p1.display();
+        p2.display();
+    }
+}
